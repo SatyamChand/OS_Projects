39bd998dab64d8eb8d4f46f5c664f5787cd77bbe
SatyamChand <satyamdeopa@gmail.com>
2020-03-19 18:00:55 +0530

    Generator pending

--- Problem4/Fibonacci.c
+++ Problem4/Fibonacci.c
@@ -5,23 +5,25 @@
 
 
 void *fibonacci_generator(void *ptr_array){
-	printf("Inside Generator\n");
 	int *arr;
 	arr=(int*)ptr_array;
-	printf("%d",sizeof(*ptr_array));
-	for(int i=0;i<5;i++){
+
+	
+
+	printf("\nThe values are : \n");
+	for(int i=1;i<=*arr;i++){
 		printf("\n%d",*(arr+i));
 	}
 }
 
 int main(){
 	pthread_t thread;
-	int value=0, *array;
-	int arr[]={1,2,3,4,6};
-	array=arr;
+	int value=0;
 	int iret;
 	printf("Please enter the number of terms you want to create in the Fibonacci series : ");
 	scanf("%d",&value);
+	int *array=calloc((value+1),sizeof(int));
+	*array=value;
 	iret=pthread_create(&thread,NULL,fibonacci_generator,(void*)array);
 	pthread_join(thread,NULL);
 	printf("\nThread1 returns : %d\n",iret);
